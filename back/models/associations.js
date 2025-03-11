12bace6dab328262b1911cdbd0f7fa50b01baacb
francososa67 <sosa44192872@gmail.com>
2025-03-11 19:24:06 -0300

    prueba

--- back/models/associations.js
+++
@@ -1,7 +0,0 @@
-const Producto = require('./producto');
-const Transaccion = require('./transaccion');
-const ProductoTransaccion = require('./productoTransaccion');
-
-// Relación muchos a muchos entre Producto y Transacción
-Producto.belongsToMany(Transaccion, { through: ProductoTransaccion, foreignKey: 'productoId' });
-Transaccion.belongsToMany(Producto, { through: ProductoTransaccion, foreignKey: 'transaccionId' });
